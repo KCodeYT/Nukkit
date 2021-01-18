feaa59d3104c2a27c3e20499b4285d358a433018
KCodeYT <KCodeYT@gmail.com>
2021-01-18 02:02:09 +0100

    Clean up ListChunkManager

--- src/main/java/cn/nukkit/level/ListChunkManager.java
+++ src/main/java/cn/nukkit/level/ListChunkManager.java
@@ -11,132 +11,121 @@ import java.util.List;
 import java.util.Optional;
 
 @Since("1.3.2.0-PN")
+@SuppressWarnings("deprecation")
 public class ListChunkManager implements ChunkManager {
 
-    private ChunkManager parent;
-    private List<Block> blocks;
+    private final Level level;
+    private final List<Block> blocks;
 
     @Since("1.3.2.0-PN")
-    public ListChunkManager(ChunkManager parent) {
-        this.parent = parent;
+    public ListChunkManager(Level parent) {
+        this.level = parent;
         this.blocks = new ArrayList<>();
-    }
-
-    @Override
-    public int getBlockIdAt(int x, int y, int z) {
-        return getBlockIdAt(x, y, z, 0);
     }
     
     private Optional<Block> findBlockAt(int x, int y, int z, int layer) {
-        return this.blocks.stream().filter(block -> 
-                block.getFloorX() == x 
-                        && block.getFloorY() == y
-                        && block.getFloorZ() == z 
-                        && block.layer == layer
-        ).findAny();
-    }
-    
-    @Override
-    public int getBlockIdAt(int x, int y, int z, int layer) {
-        return findBlockAt(x, y, z, layer).map(Block::getId).orElseGet(() -> this.parent.getBlockIdAt(x, y, z, layer));
-    }
-
-    @Override
-    public void setBlockFullIdAt(int x, int y, int z, int fullId) {
-        setBlockFullIdAt(x, y, z, 0, fullId);
-    }
-
-    @Override
-    public void setBlockFullIdAt(int x, int y, int z, int layer, int fullId) {
-        this.blocks.removeIf(block -> block.getFloorX() == x && block.getFloorY() == y && block.getFloorZ() == z && block.layer == layer);
-        this.blocks.add(Block.get(fullId, null, x, y, z, layer));
+        return this.blocks.stream().filter(block -> block.x == x && block.y == y && block.z == z && block.layer == layer).findAny();
     }
 
     @Override
     public void setBlockIdAt(int x, int y, int z, int id) {
-        setBlockIdAt(x, y, z, 0, id);
+        this.setBlockIdAt(x, y, z, 0, id);
     }
 
     @Override
     public void setBlockIdAt(int x, int y, int z, int layer, int id) {
-        Optional<Block> optionalBlock = this.blocks.stream().filter(block -> block.getFloorX() == x && block.getFloorY() == y && block.getFloorZ() == z && block.layer == layer).findAny();
-        Block block = optionalBlock.orElse(Block.get(this.getBlockIdAt(x, y, z, layer), this.getBlockDataAt(x, y, z, layer), new Position(x, y, z), layer));
-        this.blocks.remove(block);
-        this.blocks.add(Block.get(this.getBlockIdAt(x, y, z, layer), this.getBlockDataAt(x, y, z, layer), new Position(x, y, z), layer));
+        int blockData = this.getBlockDataAt(x, y, z, layer);
+        this.findBlockAt(x, y, z, layer).ifPresent(this.blocks::remove);
+        this.blocks.add(Block.get(id, blockData, this.level, x, y, z, layer));
+    }
+
+    @Override
+    public int getBlockIdAt(int x, int y, int z) {
+        return this.getBlockIdAt(x, y, z, 0);
+    }
+    
+    @Override
+    public int getBlockIdAt(int x, int y, int z, int layer) {
+        return this.findBlockAt(x, y, z, layer).map(Block::getId).orElseGet(() -> this.level.getBlockIdAt(x, y, z, layer));
+    }
+
+    @Override
+    public void setBlockDataAt(int x, int y, int z, int data) {
+        this.setBlockDataAt(x, y, z, 0, data);
+    }
+
+    @Override
+    public void setBlockDataAt(int x, int y, int z, int layer, int data) {
+        int blockId = this.getBlockIdAt(x, y, z, layer);
+        this.findBlockAt(x, y, z, layer).ifPresent(this.blocks::remove);
+        this.blocks.add(Block.get(blockId, data, this.level, x, y, z, layer));
+    }
+
+    @Override
+    public int getBlockDataAt(int x, int y, int z) {
+        return this.getBlockDataAt(x, y, z, 0);
+    }
+
+    @Override
+    public int getBlockDataAt(int x, int y, int z, int layer) {
+        return this.findBlockAt(x, y, z, layer).map(Block::getDamage).orElseGet(() -> this.level.getBlockDataAt(x, y, z, layer));
+    }
+
+    @Override
+    public void setBlockFullIdAt(int x, int y, int z, int fullId) {
+        this.setBlockFullIdAt(x, y, z, 0, fullId);
+    }
+
+    @Override
+    public void setBlockFullIdAt(int x, int y, int z, int layer, int fullId) {
+        this.findBlockAt(x, y, z, layer).ifPresent(this.blocks::remove);
+        this.blocks.add(Block.get(fullId, this.level, x, y, z, layer));
     }
 
     @Override
     public void setBlockAt(int x, int y, int z, int id, int data) {
-        this.blocks.removeIf(block -> block.getFloorX() == x && block.getFloorY() == y && block.getFloorZ() == z && block.layer == 0);
-        this.blocks.add(Block.get(id, data, new Position(x, y, z), 0));
+        this.setBlockAtLayer(x, y, z, 0, id, data);
     }
 
     @Override
     public boolean setBlockAtLayer(int x, int y, int z, int layer, int id, int data) {
-        boolean removed = this.blocks.removeIf(block -> block.getFloorX() == x && block.getFloorY() == y && block.getFloorZ() == z && block.layer == layer);
-        this.blocks.add(Block.get(id, data, new Position(x, y, z), layer));
-        return !removed;
+        this.findBlockAt(x, y, z, layer).ifPresent(this.blocks::remove);
+        return this.blocks.add(Block.get(id, data, this.level, x, y, z, layer));
     }
 
     @Since("1.4.0.0-PN")
     @PowerNukkitOnly
     @Override
     public boolean setBlockStateAt(int x, int y, int z, int layer, BlockState state) {
-        boolean removed = this.blocks.removeIf(block -> block.getFloorX() == x && block.getFloorY() == y && block.getFloorZ() == z && block.layer == layer);
-        this.blocks.add(state.getBlock(new Position(x, y, z), layer));
-        return !removed;
+        this.findBlockAt(x, y, z, layer).ifPresent(this.blocks::remove);
+        return this.blocks.add(state.getBlock(this.level, x, y, z, layer));
     }
 
     @Since("1.4.0.0-PN")
     @PowerNukkitOnly
     @Override
     public BlockState getBlockStateAt(int x, int y, int z, int layer) {
-        return findBlockAt(x, y, z, layer).map(Block::getCurrentState).orElseGet(()-> parent.getBlockStateAt(x, y, z, layer));
-    }
-
-    @Override
-    public int getBlockDataAt(int x, int y, int z) {
-        return getBlockIdAt(x, y, z, 0);
-    }
-
-    @Override
-    public int getBlockDataAt(int x, int y, int z, int layer) {
-        Optional<Block> optionalBlock = this.blocks.stream().filter(block -> block.getFloorX() == x && block.getFloorY() == y && block.getFloorZ() == z && block.layer == layer).findAny();
-        return optionalBlock.map(Block::getDamage).orElseGet(() -> this.parent.getBlockDataAt(x, y, z, layer));
-    }
-
-    @Override
-    public void setBlockDataAt(int x, int y, int z, int data) {
-        setBlockIdAt(x, y, z, 0, data);
-    }
-    
-    @Override
-    public void setBlockDataAt(int x, int y, int z, int layer, int data) {
-        Optional<Block> optionalBlock = this.blocks.stream().filter(block -> block.getFloorX() == x && block.getFloorY() == y && block.getFloorZ() == z && block.layer == layer).findAny();
-        Block block = optionalBlock.orElse(Block.get(this.getBlockIdAt(x, y, z, layer), this.getBlockDataAt(x, y, z, layer), new Position(x, y, z), layer));
-        this.blocks.remove(block);
-        block.setDamage(data);
-        this.blocks.add(block);
+        return this.findBlockAt(x, y, z, layer).map(Block::getCurrentState).orElseGet(() -> this.level.getBlockStateAt(x, y, z, layer));
     }
 
     @Override
     public BaseFullChunk getChunk(int chunkX, int chunkZ) {
-        return this.parent.getChunk(chunkX, chunkZ);
+        return this.level.getChunk(chunkX, chunkZ);
     }
 
     @Override
     public void setChunk(int chunkX, int chunkZ) {
-        this.parent.setChunk(chunkX, chunkZ);
+        this.level.setChunk(chunkX, chunkZ);
     }
 
     @Override
     public void setChunk(int chunkX, int chunkZ, BaseFullChunk chunk) {
-        this.parent.setChunk(chunkX, chunkZ, chunk);
+        this.level.setChunk(chunkX, chunkZ, chunk);
     }
 
     @Override
     public long getSeed() {
-        return this.parent.getSeed();
+        return this.level.getSeed();
     }
 
     @Since("1.3.2.0-PN")
